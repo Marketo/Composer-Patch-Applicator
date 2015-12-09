599356c07987de9a4148eac5a24588a7eebde098
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-12-09 21:09:43 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ApplyPatches
+
+A Composer post-update/install-cmd script to automatically apply patches located, 
+by default, in mysite/patches (obviously, based on an SS layout). This 
+can be altered by setting the `marketo_patch_dir` environment variable. 
+
+## Install instructions
+
+### Add it to your project with:
+
+`composer require marketo/patch-applicator`
+
+### Add the following to your composer.json
+
+```
+"scripts": {
+     "post-update-cmd": "Marketo\\CliTools\\ApplyPatches::run()",
+     "post-install-cmd": "Marketo\\CliTools\\ApplyPatches::run()"
+}
+```
---
+++ src/Marketo/CliTools/ApplyPatches.php
@@ -0,0 +1,32 @@
+<?php
+/* All code covered by the BSD license located at http://silverstripe.org/bsd-license/ */
+
+namespace Marketo\CliTools;
+
+use Composer\Script\Event;
+
+/**
+ * Task for applying patch files from a folder to a source trunk
+ *
+ * Assumes that the patches were created relative to the root folder of the application, and
+ * can be applied using patch -p0
+ *
+ * @author Marcus Nyeholt <marcus@silverstripe.com.au>
+ */
+class ApplyPatches {
+	const DEFAULT_DIR = 'mysite/patches';
+	
+	public static function run(Event $event) {
+		$patchDir = isset($_ENV['marketo_patch_dir']) ? $_ENV['marketo_patch_dir'] : getcwd() . DIRECTORY_SEPARATOR . static::DEFAULT_DIR;
+		$patches = glob($patchDir . '/*.patch');
+		
+		if (count($patches)) {
+			foreach ($patches as $patchFile) {
+				$event->getIO()->writeError('<info>Applying patches from $patchFile </info>', false);
+				$exec = "patch -r - -p0 --no-backup-if-mismatch -i " . escapeshellarg($patchFile);
+				$output = `$exec`;
+				$event->getIO()->writeError('<info>' . $output . '</info>', false);
+			}
+		}
+	}
+}
